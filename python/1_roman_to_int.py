60ea2def273094951c5e5f1179c5daa52ce81286
Hana-esf <hanaesfandiar@yahoo.com>
2022-08-23 00:02:42 +0430

    question 1 has been completed

--- python/1_roman_to_int.py
+++ python/1_roman_to_int.py
@@ -1,16 +1,26 @@
 def romanToInt(roman_nom):
-    roman_numbers = {'I':1 , 'V':5 , 'X':10 , 'L':50 , 'C':100 , 'D':500 , 'M':1000}
+        roman_numbers = {'I':1 , 'V':5 , 'X':10 , 'L':50 , 'C':100 , 'D':500 , 'M':1000}
+        integers = [roman_numbers[i] for i in roman_nom]
+        result_int = 0
+        i = 0
+        while i < len(roman_nom):
+            if i+1 < len(roman_nom):
+                if integers[i] >= integers[i+1]:
+                    result_int = result_int + integers[i]
+                    i += 1
+                else:
+                    temp_int = 0
+                    temp_int = integers[i+1] - integers[i]
+                    print(temp_int)
+                    result_int += temp_int
+                    i += 2
+            else:
+                result_int += integers[i]
+                i += 1
+        return result_int
 
-    integer = roman_numbers[roman_nom[0]]
-    print('int : ',integer)
-    for i in range(1,len(roman_nom)):
-        print(roman_nom[i],' , ',roman_numbers[roman_nom[i]])
-        if roman_numbers[roman_nom[i]] > roman_numbers[roman_nom[i-1]]:
-            integer = roman_numbers[roman_nom[i]] - integer
-        else:
-            integer += roman_numbers[roman_nom[i]]
 
-    return integer
+   
 
 
 #main
